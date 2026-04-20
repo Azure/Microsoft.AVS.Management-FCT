62134cf7b98bc5451a6b2681e732e1ffd09f1098
larabail <larabailen.lb@gmail.com>
2026-04-20 09:03:08 -0700

    set isfirst run to true to redeploy

--- ldaps_setup.ps1
+++ ldaps_setup.ps1
@@ -38,10 +38,10 @@ function Install-LDAPSCertificate {
         -NotAfter (Get-Date).AddYears(2) `
         -KeyAlgorithm RSA `
         -KeyLength 2048 `
+        -HashAlgorithm SHA256 `
         -KeyUsage DigitalSignature, KeyEncipherment `
         -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.1") `
-        -KeyExportPolicy Exportable `
-        -Provider "Microsoft RSA SChannel Cryptographic Provider"
+        -KeyExportPolicy Exportable
 
     Write-Host "[Install-LDAPSCertificate] Certificate created with thumbprint: $($Certificate.Thumbprint)"
 
